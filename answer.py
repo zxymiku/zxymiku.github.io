68a306ebdd47abe75c21ec924a4e1a7c9c29a483
zxymiku <zxymiku@outlool.com>
2024-02-23 22:52:30 +0800

    finished 现在是2024.2.23 或许未来加点难度 真能能把网页发出去吗( {{{(>_<)}}}

---
+++ answer.py
@@ -0,0 +1,8 @@
+SIXTEENHEX = "504f56474c4f5620" #一段16进制字符串 有什么用呢?
+print(SIXTEENHEX)
+import base64 #导入base64模块
+bytes_data = bytes.fromhex(SIXTEENHEX)
+#将16进制字符串转换为bytes
+# base64_encoded_string = base64.b64encode(bytes_data).decode('utf-8')
+#使用base64编码表示
+#都提示到这里了 往下我也不知道了哦（〃｀ 3′〃）
